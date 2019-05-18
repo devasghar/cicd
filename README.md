923edb14fe8391da03d51c79e3a162c2b04f239d
devasghar <dev.asghar@gmail.com>
2019-05-18 10:39:31 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+CICD
+=========================
+
+Library intended for continuous integration and
+continuous delivery.
+
+Features
+--------
+
+* PSR-4 autoloading.
---
+++ src/Main.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * @file
+ *  Contains main settings of the package
+ */
+namespace devasghar\cicd;
+class Main {
+    /*
+     * @method
+     *  constructor
+     */
+    public function __construct() {
+        //This is a blank package
+    }
+}
